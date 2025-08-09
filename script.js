e8254cdd6ec84eb5f698228ed6ef3254c6356368
PILU BHAI <BHAGYABRATAGANTAYAT@GMAIL.com>
2025-08-09 18:30:49 +0530

    game almost done only one time run purperly reset function is adding soon

--- script.js
+++ script.js
@@ -1,77 +1,93 @@
-let gameSeq=[];
-let userseq=[];
+let gameSeq = [];
+let userseq = [];
 
-let btns=['green', 'red','yellow','blue'];
+let btns = ["green", "red", "yellow", "blue"];
 
-let started=false;
-let level=0;
+let started = false;
+let level = 0;
 
-let h2=document.querySelector("h2");
+let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function(){
-    if(started == false){
-        console.log("game started");
-        started=true;
-        levelup();
-    }
+document.addEventListener("keypress", function () {
+  if (started == false) {
+    console.log("game started");
+    // body.style.backgroundColor="white";    // extra  ......
+    started = true;
+    levelup();
+  }
 });
 
-    function gameflash(btn){
-        btn.classList.add("flash");
-        setTimeout(function(){
-            btn.classList.remove("flash")
-        },240);
-    }
+function gameflash(btn) {
+  btn.classList.add("flash");
+  setTimeout(function () {
+    btn.classList.remove("flash");
+  }, 240);
+}
 
-    function userflash(btn){
-        btn.classList.add("userflash");
-        setTimeout(function(){
-        btn.classList.remove("userflash")
-        },240);
-    }
+function userflash(btn) {
+  btn.classList.add("userflash");
+  setTimeout(function () {
+    btn.classList.remove("userflash");
+  }, 240);
+}
 //   game btn press func
-    function levelup(){
-    userseq= [];
-    level++;
-    h2.innerText=`level ${level}`
-    let randIndex=Math.floor(Math.random() * 3);
-    let randcolor=btns[randIndex];
-    let randbtn = document.querySelector(`.${randcolor}`);
-    gameflash(randbtn);
-    // console.log(randIndex);
-    // console.log(randcolor);  
-    // console.log(randbtn);
-    gameSeq.push(randcolor);
-    console.log(gameSeq);
-    }
+function levelup() {
+  userseq = [];    // empty after game new flash 
+  level++;
+  h2.innerText = `level ${level}`;
+  let randIndex = Math.floor(Math.random() * 3);   // generate random number 0 to 3 for index
+  let randcolor = btns[randIndex];   
+  let randbtn = document.querySelector(`.${randcolor}`);
+  gameflash(randbtn);
+  // console.log(randIndex);
+  // console.log(randcolor);
+  // console.log(randbtn);
+  gameSeq.push(randcolor);
+  console.log(gameSeq); // for game sequence value show
+}
 
-    // func for check ans usercolor or game color
-    function checkans(idx){
-        
-        if(userseq[idx] === gameSeq[idx]){
-            if(userseq.length == gameSeq.length){
-                setTimeout(levelup, 1000)
-                // levelup();
-            }
-            // console.log("same value");
-        } else {
-            h2.innerText=`Game Over! press any key to start ` //${level}
-        }
+// func for check ans usercolor or game color
+function checkans(idx) {
+  if (userseq[idx] === gameSeq[idx]) {
+    if (userseq.length == gameSeq.length) {
+      setTimeout(levelup, 1000);
+      // levelup();
     }
+    // console.log("same value");
+  } else {
+    h2.innerHTML = `Game Over! Your Score was ${
+      level * 10
+    } <br> Press any key to start. `; //${level}
+    document.querySelector("body").style.backgroundColor = "red"; // game over warning
 
-    // user btn press
-    function btnpress(){
-        console.log(this);
-      let btn = this;
-      userflash(btn);
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white"; // returen after 1 mili second to first stage red to ehite
+    }, 150);
 
-      let usercolor=btn.getAttribute("id");
-      userseq.push(usercolor);
+    reset();
+  }
+}
 
-      checkans(userseq.length-1);
-    }
-    
-    let allbtns=document.querySelectorAll(".btn");
-    for(btn of allbtns){
-        btn.addEventListener("click", btnpress);
-    }
+// user btn press
+function btnpress() {
+  // console.log(this);  //for usr input out put
+  let btn = this;
+  userflash(btn);
+
+  let usercolor = btn.getAttribute("id");
+  userseq.push(usercolor);
+
+  checkans(userseq.length - 1);
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for (btn of allbtns) {
+  btn.addEventListener("click", btnpress);
+}
+
+function reset() {
+  started = false;
+  gameSeq = [];
+  userseq = [];
+  level = 0;
+}
