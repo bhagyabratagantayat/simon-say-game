7e0c5248ebef9d0f5fd97cc6269eca16ab2f0b9b
PILU BHAI <BHAGYABRATAGANTAYAT@GMAIL.com>
2025-08-12 15:21:22 +0530

    Final touch, add footer, and sound effects

--- script.js
+++ script.js
@@ -55,13 +55,13 @@ function checkans(idx) {
     }
     // console.log("same value");
   } else {
-    h2.innerHTML = `Game Over! Your Score was ${
+    h2.innerHTML = `Game Over! Your Score was<b> ${
       level * 10
-    } <br> Press any key to start. `; //${level}
+    } </b><br> Press any key to start. `; //${level}
     document.querySelector("body").style.backgroundColor = "red"; // game over warning
 
     setTimeout(function () {
-      document.querySelector("body").style.backgroundColor = "white"; // returen after 1 mili second to first stage red to ehite
+      document.querySelector("body").style.backgroundColor = "#87ceeb"; // returen after 1 mili second to first stage red to ehite
     }, 150);
 
     reset();
@@ -90,6 +90,41 @@ function reset() {
   gameSeq = [];
   userseq = [];
   level = 0;
+  
+
 }
+
+// adding song on click button
+// let btn1=document.querySelector(".green");
+// let btn2=document.querySelector(".green");
+// let btn3=document.querySelector(".green");
+// let btn4=document.querySelector(".green");
+
+// btn1.addEventListener("click", function(){
+//   let audio = new Audio("songs/song 4.mp3"); 
+//     audio.play(); // Play the music 
+// });
+
+
 // made with love pilu bhai 
+
+
+let songs = document.querySelectorAll("audio");
+
+//play first song on button click
+document.querySelector(".green").addEventListener("click", function () {
+    songs[0].play(); // plays "bit 1.mp3"
+});
+
+document.querySelector(".red").addEventListener("click", function () {
+    songs[1].play(); // plays "bit 2.mp3"
+});
+
+document.querySelector(".yellow").addEventListener("click", function () {
+    songs[2].play(); // plays "bit 3.mp3"
+});
+
+document.querySelector(".blue").addEventListener("click", function () {
+    songs[3].play(); // plays "bit 4.mp3"
+});
 
